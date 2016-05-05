a3715feb4874c12a9d36126d6527a39c3b3e8b5e
Akshay Darekar <acdarekar@gmail.com>
2016-05-05 20:58:40 +0530

    incomplete: calculate background size

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs-extra'));
 var path = require('path');
 
+// Need to calculate background-size for larger image.
+
+// background-size: (last_index * 64px) 64px;
+
+// get last index
+Handlebars.registerHelper('last_index', function(context, options) {
+  return context.length;
+});
+
+// register multiplication helper
+Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+    lvalue = parseFloat(lvalue);
+    rvalue = parseFloat(rvalue);
+
+    return {
+        "*": lvalue * rvalue
+    }[operator];
+});
+
 var getTemplate = function () {
   return fs.readFileAsync(path.join(__dirname, 'template', 'less.hbs'), 'utf8');
 };
