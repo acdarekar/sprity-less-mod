d50a6ae95e24fd3c440745cbca290fd4187fdf3b
Akshay Darekar <acdarekar@gmail.com>
2016-05-06 14:42:12 +0530

    write function to calculate the total width for 64*64 sprite

--- index.js
+++ index.js
@@ -4,25 +4,6 @@ var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs-extra'));
 var path = require('path');
 
-// Need to calculate background-size for larger image.
-
-// background-size: (last_index * 64px) 64px;
-
-// get last index
-Handlebars.registerHelper('last_index', function(context, options) {
-  return context.length;
-});
-
-// register multiplication helper
-Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
-    lvalue = parseFloat(lvalue);
-    rvalue = parseFloat(rvalue);
-
-    return {
-        "*": lvalue * rvalue
-    }[operator];
-});
-
 var getTemplate = function () {
   return fs.readFileAsync(path.join(__dirname, 'template', 'less.hbs'), 'utf8');
 };
@@ -37,6 +18,23 @@ var transform = Promise.method(function (layouts, source, opt, Handlebars) {
 
 module.exports = {
   process: function (layouts, opt, Handlebars) {
+    Handlebars.registerHelper('length', function(layouts, sprites, options) {
+      return context.length;
+    });
+
+    Handlebars.registerHelper('math', function(lvalue, operator, rvalue, options) {
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+
+        return {
+          "+": lvalue + rvalue,
+          "-": lvalue - rvalue,
+          "*": lvalue * rvalue,
+          "/": lvalue / rvalue,
+          "%": lvalue % rvalue
+        }[operator];
+    });
+
     return getTemplate()
       .then(function (source) {
         return transform(layouts, source, opt, Handlebars);
